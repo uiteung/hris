f13e062135c325782969133e4d483d22c1d87309
rofinafiin <rofinafiisr@gmail.com>
2023-01-27 10:12:30 +0700

    tabel gaji

--- js/keuangan.js
+++ js/keuangan.js
@@ -1,9 +1,33 @@
 var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://hris_backend.ulbi.ac.id/gaji/peg", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://hris_backend.ulbi.ac.id/gaji/peg/transaksiall", requestOptions)
+.then((result)=>{
+  return result.json();
+})
+.then((objectData)=>{
+  // console.log(objectData)
+  console.log(objectData.data[0].nip);
+  let tableData="";
+  objectData.data.map((values)=>{
+      tableData +=`
+      <div>
+      <tr class="text-xs">
+      <td class="flex items-center py-5 px-6 font-medium">
+        <input class="mr-3" type="checkbox" name="" id="">
+        <p>${values.nik}</p>
+      </td>
+      <td class="font-medium">${values.pokok}</td>
+      <td class="font-medium">${values.gaji_kotor}</td>
+      <td>
+      ${values.total_potongan}
+      </td>
+      <td>${values.gaji_bersih}</td>
+    </tr>
+    </div>`;
+  });
+  document.getElementById("tablecrot").innerHTML=tableData
+})
+.catch(error => console.log('error', error));
