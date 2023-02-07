9adcddfd488feb902e1fc9eb53d43980bd01801d
rofinafiin <rofinafiisr@gmail.com>
2023-02-07 10:58:09 +0700

    fix

--- js/insert.js
+++ js/insert.js
@@ -21,8 +21,6 @@
       redirect: 'follow'
     };
     
-
-
     console.log(requestOptions);
   
     fetch("https://hris_backend.ulbi.ac.id/gaji/peg", requestOptions)
@@ -31,55 +29,7 @@
     .catch(error => console.log('error', error));
 
   })
-  // const form = document.getElementById('MyForm');
 
-  // form.addEventListener('submit', function(e) {
-  //   e.preventDefault()
-
-  //   var nik = document.getElementById('nik').value
-  //   var pokok = document.getElementById('pokok').value
-  //   var keluarga = document.getElementById('keluarga').value
-  //   var pangan = document.getElementById('pangan').value
-  //   var kinerja = document.getElementById('kinerja').value
-  //   var keahlian = document.getElementById('keahlian').value
-  //   var struktural = document.getElementById('struktural').value
-  //   var transportasi = document.getElementById('transportasi').value
-  //   var kehadiran = document.getElementById('kehadiran').value
-  //   var kopkar = document.getElementById('kopkar').value
-  //   var bankjabar = document.getElementById('bankjabar').value
-  //   var bpjs = document.getElementById('bpjs').value
-  //   var arisan = document.getElementById('arisan').value
-  //   var bauk = document.getElementById('bauk').value
-  //   var pph = document.getElementById('pph').value
-  //   var lain2 = document.getElementById('lain2').value
-
-  //   var raw = JSON.stringify({
-  //     "nik": nik,
-  //     "pokok": pokok,
-  //     "keluarga": keluarga,
-  //     "pangan": pangan,
-  //     "kinerja": kinerja,
-  //     "keahlian": keahlian,
-  //     "struk": struktural,
-  //     "transportasi": transportasi,
-  //     "kehadiran": kehadiran,
-  //     "kopkar": kopkar,
-  //     "bank_jabar": bankjabar,
-  //     "bpjs": bpjs,
-  //     "arisan": arisan,
-  //     "bauk": bauk,
-  //     "pph": pph,
-  //     "lain2": lain2
-  //   });
-    
-
-  
-  // fetch("https://hris_backend.ulbi.ac.id/gaji/peg", requestOptions)
-  // .then(response => response.json())
-  // .then(result => console.log(result))
-  // .catch(error => console.log('error', error));
-  // console.log(raw)
-  // })
 
 
 
