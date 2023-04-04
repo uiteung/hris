251497b88289ed8fdaf0846e53dc1ec5ddf58370
Valen Rionald <vrionald@gmail.com>
2023-04-04 12:37:49 +0700

    Fix FE

--- js/detailpegawai.js
+++ js/detailpegawai.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const idSdm = urlParams.get('id_sdm');
 
 // Ambil data pegawai dengan mengirimkan request ke endpoint "https://hris_backend.ulbi.ac.id/pegawai/sdm/byid/:id_sdm"
-fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/byid/${idSdm}`)
+fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/profilpribadi/${idSdm}`)
 	.then((result) => {
 		// Ubah data yang didapat dari server menjadi objek JSON
 		return result.json();
@@ -18,35 +18,35 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/byid/${idSdm}`)
                   <div class="col-sm-3">
                     <h6 class="mb-0">Nama Lengkap</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.nama}</div>
+                  <div class="col-sm-9 text-bold">${data.data.nama}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Jenis Kelamin</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.jenis_kelamin}</div>
+                  <div class="col-sm-9 text-bold">${data.data.jenis_kelamin}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Tempat Lahir</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.tempat_lahir}</div>
+                  <div class="col-sm-9 text-bold">${data.data.tempat_lahir}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Tanggal Lahir</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.tanggal_lahir}</div>
+                  <div class="col-sm-9 text-bold">${data.data.tanggal_lahir}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Nama Ibu Kandung</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.nama_ibu_kandung}</div>
+                  <div class="col-sm-9 text-bold">${data.data.nama_ibu_kandung}</div>
                 </div>
 			</div>`;
 		// Tampilkan data pegawai ke dalam tabel
@@ -65,37 +65,37 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/kepegawaian/${idSdm}`)
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">NIP</h6>
-                  <span class="text-bold">${data.nip}</span>
+                  <span class="text-bold">${data.data.nip}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">SK CPNS</h6>
-                  <span class="text-bold">${data.sk_cpns}</span>
+                  <span class="text-bold">${data.data.sk_cpns}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">Tanggal SK CPNS</h6>
-                  <span class="text-bold">${data.TanggalSkCpns}</span>
+                  <span class="text-bold">${data.data.TanggalSkCpns}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">SK TMMD</h6>
-                  <span class="text-bold">${data.sk_tmmd}</span>
+                  <span class="text-bold">${data.data.sk_tmmd}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">TMMD</h6>
-                  <span class="text-bold">${data.tmmd}</span>
+                  <span class="text-bold">${data.data.tmmd}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">Sumber Gaji</h6>
-                  <span class="text-bold">${data.sumber_gaji}</span>
+                  <span class="text-bold">${data.data.sumber_gaji}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">NIDN</h6>
-                  <span class="text-bold">${data.nidn}</span>
+                  <span class="text-bold">${data.data.nidn}</span>
                 </li>
               </ul>
             </div>
@@ -115,12 +115,12 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/lain/${idSdm}`)
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">NPWP</h6>
-                  <span class="text-bold">${data.npwp}</span>
+                  <span class="text-bold">${data.data.npwp}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 class="mb-0">Nama</h6>
-                  <span class="text-bold">${data.nama_wp}</span>
+                  <span class="text-bold">${data.data.nama_wp}</span>
                 </li>
               </ul>
             </div>
@@ -139,22 +139,22 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/kependudukan/${idSdm}`)
 					<hr />
                     <h6 class="mb-0">NIK</h6>
                     <div>
-                      <h7>${data.nik}</h7>
+                      <h7>${data.data.nik}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">Agama</h6>
                     <div>
-                      <h7>${data.agama}</h7>
+                      <h7>${data.data.agama}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">Kewarganegaraan</h6>
                     <div>
-                      <h7>${data.kewarganegaraan}</h7>
+                      <h7>${data.data.kewarganegaraan}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">Kode Negara</h6>
                     <div>
-                      <h7>${data.kode_negara}</h7>
+                      <h7>${data.data.kode_negara}</h7>
                     </div>
 		`;
 		document.getElementById("kependudukan").innerHTML = divData;
@@ -171,22 +171,22 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/keluarga/${idSdm}`)
 					<hr />
                     <h6 class="mb-0">Status Kawin</h6>
                     <div>
-                      <h7>${data.status_kawin}</h7>
+                      <h7>${data.data.status_kawin}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">Nama Pasangan</h6>
                     <div>
-                      <h7>${data.nama_pasangan}</h7>
+                      <h7>${data.data.nama_pasangan}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">NIP Pasangan</h6>
                     <div>
-                      <h7>${data.nip_pasangan}</h7>
+                      <h7>${data.data.nip_pasangan}</h7>
                     </div>
 					<hr />
                     <h6 class="mb-0">Pekerjaan Pasangan</h6>
                     <div>
-                      <h7>${data.pekerjaan_pasangan}</h7>
+                      <h7>${data.data.pekerjaan_pasangan}</h7>
                     </div>
 		`;
 		document.getElementById("keluarga").innerHTML = divData;
@@ -198,7 +198,7 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/bidang_ilmu/${idSdm}`)
 	})
 	.then((data) => {
 		let tableData = "";
-		data.forEach((data) => {
+		data.data.map((data) => {
 			tableData += `
 	                <tr>
 	                  <td>${data.urutan}</td>
@@ -224,70 +224,70 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/alamat/${idSdm}`)
                   <div class="col-sm-3">
                     <h6 class="mb-0">Email</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.email}</div>
+                  <div class="col-sm-9 text-bold">${data.data.email}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Alamat</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.alamat}</div>
+                  <div class="col-sm-9 text-bold">${data.data.alamat}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Rt</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.rt}</div>
+                  <div class="col-sm-9 text-bold">${data.data.rt}</div>
                 </div>
                 <hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Rw</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.rw}</div>
+                  <div class="col-sm-9 text-bold">${data.data.rw}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Dusun</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.dusun}</div>
+                  <div class="col-sm-9 text-bold">${data.data.dusun}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Kelurahan</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.kelurahan}</div>
+                  <div class="col-sm-9 text-bold">${data.data.kelurahan}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Kota / Kabupaten</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.kota_kabupaten}</div>
+                  <div class="col-sm-9 text-bold">${data.data.kota_kabupaten}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Kode Pos</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.kode_pos}</div>
+                  <div class="col-sm-9 text-bold">${data.data.kode_pos}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Telepon Rumah</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.telepon_rumah}</div>
+                  <div class="col-sm-9 text-bold">${data.data.telepon_rumah}</div>
                 </div>
 				<hr />
                 <div class="row">
                   <div class="col-sm-3">
                     <h6 class="mb-0">Telepon Hp</h6>
                   </div>
-                  <div class="col-sm-9 text-bold">${data.telepon_hp}</div>
+                  <div class="col-sm-9 text-bold">${data.data.telepon_hp}</div>
                 </div>
 			</div>`;
 		// Tampilkan data pegawai ke dalam tabel
@@ -300,7 +300,7 @@ fetch(`https://hris_backend.ulbi.ac.id/pegawai/sdm/ajuan?id_sdm=${idSdm}`)
 	})
 	.then((data) => {
 		let tableData = "";
-		data.forEach((data) => {
+		data.data.map((data) => {
 			tableData += `
 	                <tr>
 	                  <td>${data.tanggal_ajuan}</td>
--- js/pegawai.js
+++ js/pegawai.js
@@ -1,14 +1,12 @@
-// Ambil data pegawai dengan mengirimkan request ke endpoint "https://hris_backend.ulbi.ac.id/pegawai/sdm"
 fetch("https://hris_backend.ulbi.ac.id/pegawai/sdm")
-	.then((result) => {
-		// Ubah data yang didapat dari server menjadi objek JSON
-		return result.json();
-	})
-	.then((data) => {
-		let tableData = "";
-		// Looping data pegawai dan masukkan ke dalam bentuk tabel
-		data.map((values) => {
-			tableData += `
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    let tableData = "";
+    data.data.map((values) => {
+      // Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
+      tableData += `
         <tr class="text-xs bg-gray-50">
           <td class="py-5 px-6 font-medium">${values.nidn}</td>
           <td class="flex px-4 py-3">
@@ -29,26 +27,24 @@ fetch("https://hris_backend.ulbi.ac.id/pegawai/sdm")
           </td>
           <td>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-employee-id="${values.id_sdm}">
-			Detail
-			</button>
-
+              Detail
+            </button>
           </td>
         </tr>`;
-		});
-		// Tampilkan data pegawai ke dalam tabel
-		document.getElementById("tablebody").innerHTML = tableData;
+    });
+    // Tampilkan data pegawai ke dalam tabel
+    document.getElementById("tablebody").innerHTML = tableData;
 
-		// Tambahkan event listener pada setiap tombol detail untuk mengambil id_sdm dan mengarahkan ke halaman detail pegawai yang sesuai
-		const detailButtons = document.querySelectorAll('.bg-blue-500');
-		detailButtons.forEach((button) => {
-			button.addEventListener('click', (event) => {
-				const idSdm = event.target.getAttribute('data-employee-id');
-				window.location.href = `detailpegawai.html?id_sdm=${idSdm}`;
-			});
-		});
-	})
-	.catch(error => {
-		// Tangani error jika terjadi
-		console.log('error', error);
-		alert('Terjadi kesalahan pada server');
-	});
+    // Tambahkan event listener pada setiap tombol detail untuk mengambil id_sdm dan mengarahkan ke halaman detail pegawai yang sesuai
+    const detailButtons = document.querySelectorAll('.bg-blue-500');
+    detailButtons.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        const idSdm = event.target.getAttribute('data-employee-id');
+        window.location.href = `detailpegawai.html?id_sdm=${idSdm}`;
+      });
+    });
+  })
+  .catch(error => {
+    console.log('error', error);
+    alert('Terjadi kesalahan pada server');
+  });
