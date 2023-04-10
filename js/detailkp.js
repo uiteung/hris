67cee75a808d956deaaa5604c4ce11cc0930667a
Valen Rionald <vrionald@gmail.com>
2023-04-10 16:25:28 +0700

    Progress KP

---
+++ js/detailkp.js
@@ -0,0 +1,177 @@
+const urlParams = new URLSearchParams(window.location.search);
+const idNaikpangkat = urlParams.get("id_naikpangkat");
+
+fetch(`http://127.0.0.1:3000/kp/kenaikanpangkat/byid/${idNaikpangkat}`)
+  .then((result) => {
+    // Ubah data yang didapat dari server menjadi objek JSON
+    return result.json();
+  })
+  .then((data) => {
+    // Tampilkan data pegawai ke dalam tabel atau div sesuai dengan struktur HTML yang digunakan pada file detailpegawai.html
+    let divData = "";
+    // Looping data pegawai dan masukkan ke dalam bentuk tabel
+    divData += `
+            <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Nama</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.nama}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">NIDN</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.nidn}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">NIP</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.nip}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">NIPY</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.nipy}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">NIK</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.ktp}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">No. Handphone</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.handphone}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Golongan</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.gol}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Jabatan</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.singkatan}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Jenis Pegawai</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.jns_kepeg}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Status Pegawai</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.status}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">No. Surat Usul</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.no_surat_usul}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Tanggal Surat Usul</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.tgl_surat_usul}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">No. SK</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.no_sk}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Tanggal SK</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.tgl_sk}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Pejabat SK</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.pejabat_sk}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Masa Kerja (Tahun)</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.mk_tahun}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Masa Kerja (Bulan)</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.mk_bulan}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Terhitung Mulai Tanggal Baru</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.tmt_baru}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Gaji Pokok Baru</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.gapok_baru}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Gaji Pokok Terbilang</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.gapok_terbilang}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Naik Selanjutnya</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.naik_selanjutnya}</div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Keterangan</h6>
+                  </div>
+                  <div class="col-sm-9 text-bold">${data.data.ket}</div>
+                </div>
+			</div>`;
+    // Tampilkan data pegawai ke dalam tabel
+    document.getElementById("detailkp").innerHTML = divData;
+  })
+
+  .catch((error) => {
+    // Tangani error jika terjadi
+    console.log("error", error);
+    alert("Terjadi kesalahan pada server");
+  });
--- js/kenaikanpangkat.js
+++ js/kenaikanpangkat.js
@@ -1,4 +1,4 @@
-fetch("https://hris_backend.ulbi.ac.id/kp/kenaikanpangkat")
+fetch("http://127.0.0.1:3000/kp/kenaikanpangkat")
   .then((result) => {
     return result.json();
   })
@@ -8,25 +8,19 @@ fetch("https://hris_backend.ulbi.ac.id/kp/kenaikanpangkat")
       // Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
       tableData += `
         <tr class="text-xs bg-gray-50">
+          <td class="py-5 px-6 font-medium">${values.nama}</td>
           <td class="py-5 px-6 font-medium">${values.nip}</td>
-          <td class="py-5 px-6 font-medium">${values.nik}</td>
-          <td class="font-medium">${values.mk_bulan}</td>
-          <td class="font-medium">${values.mk_tahun}</td>
-          <td class="font-medium">${values.no_sk}</td>
-          <td class="font-medium">${values.tgl_sk}</td>
-          <td class="font-medium">${values.no_surat_usul}</td>
-          <td class="font-medium">${values.tgl_surat_usul}</td>
-          <td class="font-medium">${values.naik_selanjutnya}</td>
-          <td>
-          <span class="inline-block py-1 px-2 text-white bg-green-500 rounded-full">${values.pejabat_sk}</span>
+          <td class="py-5 px-6 font-medium">${values.nipy}</td>
+          <td class="py-5 px-6 font-medium">${values.ktp}</td>
+          <td class="py-5 px-6 font-medium">${values.nidn}</td>
+          <td class="py-5 px-6 font-medium">${values.handphone}</td>
+          <td class="py-5 px-6 font-medium">
+            <span class="inline-block py-1 px-2 text-white bg-green-500 rounded-full">${values.status}</span>
           </td>
-          <td>
-          <span class="inline-block py-1 px-2 text-purple-500 bg-purple-50 rounded-full">${values.ket}</span>
-          </td>
-          <td>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-employee-nip="${values.nip}">
-              Naik Pangkat
-            </button>
+          <td class="py-5 px-6 font-medium">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-id-naikpangkat="${values.id_naikpangkat}">
+            Detail
+          </button>        
           </td>
         </tr>`;
     });
@@ -34,15 +28,19 @@ fetch("https://hris_backend.ulbi.ac.id/kp/kenaikanpangkat")
     document.getElementById("tablebody").innerHTML = tableData;
 
     // Tambahkan event listener pada setiap tombol detail untuk mengambil nip dan mengarahkan ke halaman detail pegawai yang sesuai
-    const detailButtons = document.querySelectorAll('.bg-blue-500');
+    const detailButtons = document.querySelectorAll(".bg-blue-500");
     detailButtons.forEach((button) => {
-      button.addEventListener('click', (event) => {
-        const nip = event.target.getAttribute('data-employee-nip');
-        window.location.href = `prosesnaikpangkat.html?nip=${nip}`;
+      button.addEventListener("click", (event) => {
+        const idNaikpangkat = event.target.getAttribute("data-id-naikpangkat");
+        if (!idNaikpangkat) {
+          alert("Terjadi kesalahan. Tidak dapat menampilkan detail data.");
+          return;
+        }
+        window.location.href = `detailkp.html?id_naikpangkat=${idNaikpangkat}`;
       });
     });
   })
-  .catch(error => {
-    console.log('error', error);
-    alert('Terjadi kesalahan pada server');
+  .catch((error) => {
+    console.log("error", error);
+    alert("Terjadi kesalahan pada server");
   });
